3618ba2a1e4570f6b32345198ce692f02bd11ad2
Satish Yadav <80574594+11Satish@users.noreply.github.com>
2021-09-18 01:34:45 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args){
+        // String
+        // concatenate
+        String name1 = "satish";
+        String name2 = "Amit";
+        String name3 = name1 + " and " + name2;
+
+        System.out.println(name3);
+    }
+}
