481c4e4f02c83784543bbcf689bef49502527897
Satish Yadav <80574594+11Satish@users.noreply.github.com>
2021-09-17 17:18:08 +0530

    Add files via upload

---
+++ Variable/Variable.java
@@ -0,0 +1,25 @@
+package com.Variable;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        // byte = 1 [-128 to 127]
+        // shot = 2
+        // int = 4 [1,2,3,4]
+        // long = 8
+        // float = 4 [3.14]
+        // double = 8
+        // char = 2
+        // boolean = 1 [true/false]
+
+        //example
+        byte age = 30;
+        int phone = 1234567890;
+        long phone2 = 12234567890000L;
+        float pi = 3.14F;
+        char letter = 's';
+        boolean isAdult = true;
+
+        System.out.println("phone");
+    }
+}
