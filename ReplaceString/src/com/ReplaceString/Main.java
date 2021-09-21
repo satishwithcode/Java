67ed9bc32c40281d42c77066ae78f12fa8d87971
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-09-22 01:59:12 +0530

    Add files via upload

---
+++ ReplaceString/src/com/ReplaceString/Main.java
@@ -0,0 +1,14 @@
+package com.ReplaceString;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // String
+        // Replace
+
+        String name = "satish";
+        String name2 = name.replace('s','0');
+
+        System.out.println(name2);
+    }
+}
---
+++ SubString/src/com/SubString/Main.java
@@ -0,0 +1,13 @@
+package com.SubString;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // String
+        // SubString
+
+        String name = "Satish And Amit";
+        System.out.println(name.substring(7,10));
+    }
+    // output : And
+}
