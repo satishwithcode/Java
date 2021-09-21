d20fdc79d2a9cd6c200d9b1df5a235624f1d532a
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-09-22 01:40:16 +0530

    Add files via upload

---
+++ ChatAtString/src/com/ChatAtString/Main.java
@@ -0,0 +1,14 @@
+package com.ChatAtString;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // String
+        // ChatAt
+        // java is a zero index base language
+
+        String name = "Satish";
+        System.out.println(name.charAt(0));
+    }
+}
+ // output : S
---
+++ LengthString/src/com/LengthString/Main.java
@@ -0,0 +1,12 @@
+package com.LengthString;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // String
+        // Length
+
+        String name = "Satish";
+        System.out.println(name.length());
+    }
+}
