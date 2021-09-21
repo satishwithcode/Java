8e62174438363cf222816709db5b547adc0661d4
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-09-22 01:28:09 +0530

    Add files via upload

---
+++ ConcatenateString/src/com/ConcatenateString/Main.java
@@ -0,0 +1,16 @@
+package com.ConcatenateString;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // String
+        // concatenate
+
+        String name1 = "satish";
+        String name2 = "Amit";
+        String name3 = name1 + " and " + name2;
+
+        System.out.println(name3);
+    }
+}
