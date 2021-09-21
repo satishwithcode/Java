5c391c36d8bea98b34ae11a57b346ac7a8017d77
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-09-22 01:20:47 +0530

    Add files via upload

---
+++ ChatAtString/src/com/ChatAtString/Main.java
@@ -0,0 +1,12 @@
+package com.ChatAtString;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // String
+        // ChatAt
+
+        String name = "Satish";
+        System.out.println(name.length());
+    }
+}
