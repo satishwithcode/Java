56dc20f1bba7ca646e9eda395c5f71d7c0adca6c
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-05 17:54:38 +0530

    Add files via upload

---
+++ 8LengthArrays/src/com/LengthArrays/Main.java
@@ -0,0 +1,16 @@
+package com.LengthArrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Arrays
+        // Length
+
+        int[] marks = new int[3];
+        marks[0] = 97;
+        marks[1] = 98;
+        marks[2] = 95;
+
+        System.out.println(marks.length);
+    }
+}
