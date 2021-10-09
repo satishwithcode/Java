fb6c196a9f67adb66a6c8cb65de6ca843df05fb5
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:18:24 +0530

    Add files via upload

---
+++ MathClass/src/com/Operators/Main.java
@@ -0,0 +1,16 @@
+package com.Operators;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Operators
+        // Maths
+
+        // 5,6
+
+        //System.out.println(Math.max(5,6));   // max
+
+        System.out.println(Math.min(5,6));    // min
+
+    }
+}
