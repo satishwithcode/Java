7e0bfc7b47100dd5a412b14220f244111fec824a
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:24:38 +0530

    Add files via upload

---
+++ WhileLoops/src/com/Loops/Main.java
@@ -0,0 +1,14 @@
+package com.Loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // While Loops
+        int i = 1;
+        while (i <= 100){
+            System.out.println(i);
+            i++; // or i = i + 1
+        }
+
+    }
+}
