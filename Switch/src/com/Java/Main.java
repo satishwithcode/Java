40cb4bd1aaaab738f54fc5ad404e7d226ceb024b
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:23:25 +0530

    Add files via upload

---
+++ Switch/src/com/Java/Main.java
@@ -0,0 +1,26 @@
+package com.Java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Switch Statements
+
+        int day = 2;
+        switch(day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tousday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("thu -- sun");
+        }
+    }
+}
