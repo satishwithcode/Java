914cb6708758ac9ce5334df10f307b7919f6d8be
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:27:32 +0530

    Break Statements

---
+++ BreakStatements/src/com/Loops/Main.java
@@ -0,0 +1,19 @@
+package com.Loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // break statements
+
+        int i = 0;
+        while (true) {
+            System.out.println(i);
+            i++;
+            if (i > 10) {
+                break;
+
+            }
+
+        }
+    }
+}
