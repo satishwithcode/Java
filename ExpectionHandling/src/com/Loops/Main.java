322be555955fda1a1589271b6b532c9375cdd40c
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:29:25 +0530

    Expection Handling

---
+++ ExpectionHandling/src/com/Loops/Main.java
@@ -0,0 +1,19 @@
+package com.Loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Expection handling
+
+        int[] marks = {97,95,99};
+
+        try {   // catch expection and write in try{}
+            System.out.println(marks[5]);
+        }
+        catch (Exception exception){
+            // do something after catching
+        }
+
+
+    }
+}
