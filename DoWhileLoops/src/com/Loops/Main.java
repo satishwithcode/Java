ffe053446ea8e8442460d9bebf142e6ee37f772e
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:25:36 +0530

    do while loops

---
+++ DoWhileLoops/src/com/Loops/Main.java
@@ -0,0 +1,15 @@
+package com.Loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Do while loops
+
+        int i = 1;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i <= 100);
+
+    }
+}
