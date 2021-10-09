4eb5b195c9250c5f0ec164ee215d42506f69813b
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-09 17:48:51 +0530

    Add files via upload

---
+++ ArithmeticOperators/src/com/Operators/Main.java
@@ -0,0 +1,17 @@
+package com.Operators;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Operators
+        // Arithmetic Operators    + , -, * , /
+
+        int a = 2;
+        int b = 3;
+        int sum = a+b;
+
+
+        System.out.println(sum);
+    }
+}
