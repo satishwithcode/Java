7ab33664f9cc4adb62fa64626e7c3d8f2f506909
Satish Yadav <80574594+SatishWithCode@users.noreply.github.com>
2021-10-10 01:24:11 +0530

    Add files via upload

---
+++ ForLoops/src/com/Loops/Main.java
@@ -0,0 +1,13 @@
+package com.Loops;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // loops
+        // For loops
+
+        for(int i = 1; i<=100; i++){
+            System.out.println(i);
+        }
+    }
+}
